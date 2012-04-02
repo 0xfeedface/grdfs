a2ee4f86cc93c6fe2a4497772e9590cadb088dee
Norman Heino <norman.heino@gmail.com>
2012-04-02 15:20:05 +0200

    Add profiling information for parsing, compression and storage.

--- src/main.cc
+++ src/main.cc
@@ -15,6 +15,7 @@
 #include <cassert>
 #include <memory>
 #include <fstream>
+#include "infra/util/Type.hpp"
 #include "cts/parser/TurtleParser.hpp"
 
 #include "types.h"
@@ -25,6 +26,12 @@
 //#undef GRDFS_PROFILING
 
 void printUsage();
+std::string const& translateType(Type::ID);
+
+static const char* typeNames[] = {
+  "URI", "Literal", "CustomLanguage", "CustomType",
+  "String", "Integer", "Decimal", "Double", "Boolean"
+};
 
 int main (int argc, const char * argv[]) {
   if (argc < 2) {
@@ -43,10 +50,16 @@ int main (int argc, const char * argv[]) {
 //  OpenCLReasoner reasoner(dictionary);
   NativeReasoner reasoner(dictionary);
 
+#ifdef GRDFS_PROFILING
+  uint64_t preParsing, parsing(0), preLookup, lookup(0), preStorage, storage(0);
+#endif
   TurtleParser parser(file);
   std::string subject, predicate, object, subType;
   Type::ID type;
   while (true) {
+#ifdef GRDFS_PROFILING
+    preParsing = mach_absolute_time();
+#endif
     try {
       if (!parser.parse(subject, predicate, object, type, subType)) {
         break;
@@ -59,14 +72,34 @@ int main (int argc, const char * argv[]) {
         continue;
       }
     }
+#ifdef GRDFS_PROFILING
+    parsing += mach_absolute_time() - preParsing;
+#endif
 
+#ifdef GRDFS_PROFILING
+    preLookup = mach_absolute_time();
+#endif
     term_id subjectID   = dictionary.Lookup(subject);
     term_id predicateID = dictionary.Lookup(predicate);
     term_id objectID    = dictionary.Lookup(object, type == Type::ID::Literal);
+#ifdef GRDFS_PROFILING
+    lookup += mach_absolute_time() - preLookup;
+#endif
 
+    /*
+     * if (type != Type::ID::URI && type != Type::ID::Literal && type != Type::ID::String) {
+     *   std::cout << object << " (" << subType << ", " << translateType(type) << ")\n";
+     * }
+     */
+
+#ifdef GRDFS_PROFILING
+    preStorage = mach_absolute_time();
+#endif
     reasoner.addTriple(triple(subjectID, predicateID, objectID));
+#ifdef GRDFS_PROFILING
+    storage += mach_absolute_time() - preStorage;
+#endif
   }
-
 
   dictionary.PrintStatistics();
 
@@ -78,9 +111,14 @@ int main (int argc, const char * argv[]) {
   uint64_t afterClosure = mach_absolute_time();
   struct mach_timebase_info info;
   mach_timebase_info(&info);
+  std::cout.setf(std::ios::fixed, std::ios::floatfield);
+  std::cout.precision(2);
   std::cout << "Closure calculation took " << 1e-6 * ((afterClosure - beforeClosure) * info.numer / info.denom) << " ms" << std::endl;
+  std::cout << "Parsing: " << 1e-6 * parsing * info.numer / info.denom << " ms\n";
+  std::cout << "Dictionary lookup: " << 1e-6 * lookup * info.numer / info.denom << " ms\n";
+  std::cout << "Storage: " << 1e-6 * storage * info.numer / info.denom << " ms\n";
 #endif
-  
+
   return EXIT_SUCCESS;
 }
 
@@ -88,5 +126,9 @@ void printUsage() {
   std::cout <<  "usage: grdfs <rdf_file.ttl>" << std::endl;
 //  std::cout <<  "usage: grdfs <rdf_file.ttl> [-p]" << std::endl;
 //  std::cout <<  "       -p: print profiling information" << std::endl;
+}
+
+std::string const& translateType(Type::ID type) {
+  return typeNames[type];
 }
 
