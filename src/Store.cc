7a35e1e28b6fbc9a2d9c59b88b9ef06ea4ec8c90
Norman Heino <norman.heino@gmail.com>
2012-04-05 22:26:16 +0200

    Add methods to copy triple elements as vector and for counting.

--- src/Store.cc
+++ src/Store.cc
@@ -13,12 +13,78 @@
 using std::cout;
 
 bool Store::addTriple(triple t) {
-  auto predicates = storage_[t.subject];
-  auto objects = predicates[t.predicate];
-  auto result = objects.insert(t.object);
-  return result.second;
+  PredicateMap& predicates = storage_[t.subject];
+  TermSet& objects = predicates[t.predicate];
+  auto result = objects.insert(t.object).second;
+  
+  if (result) {
+    ++tripleCount_;
+  }
+  
+  return result;
 }
 
-void Store::PrintStatistics() {
+void Store::printStatistics() {
   cout << "Distinct subjects: " << storage_.size() << "\n";
+}
+
+void Store::copySubjects(TermVector& subjects) {
+  for (auto value : storage_) {
+    subjects.push_back(value.first);
+  }
+}
+
+void Store::copyPredicates(TermVector& predicates) {
+  auto subjectIter(std::begin(storage_));
+  for (; subjectIter != std::end(storage_); ++subjectIter) {
+    PredicateMap preds(subjectIter->second);
+    
+    auto predicateIter(std::begin(preds));
+    for (; predicateIter != std::end(preds); ++predicateIter) {
+      term_id p(predicateIter->first);
+      TermSet objects(predicateIter->second);
+      // Insert p as many times as there are objects for it
+      predicates.insert(predicates.cend(), objects.size(), p);
+    }
+  }
+}
+
+void Store::copyObjects(TermVector& objectsTarget) {
+  auto subjectIter(std::begin(storage_));
+  for (; subjectIter != std::end(storage_); ++subjectIter) {
+    PredicateMap predicates(subjectIter->second);
+    
+    auto predicateIter(std::begin(predicates));
+    for (; predicateIter != std::end(predicates); ++predicateIter) {
+      TermSet objects(predicateIter->second);
+      
+      auto objectIter(std::begin(objects));
+      for (; objectIter != std::end(objects); ++objectIter) {
+        term_id o(*objectIter);
+        
+        objectsTarget.push_back(o);
+      }
+    }
+  }
+}
+
+void Store::copyTriples(TripleVector& triples) {
+  auto subjectIter(std::begin(storage_));
+  for (; subjectIter != std::end(storage_); ++subjectIter) {
+    term_id s(subjectIter->first);
+    PredicateMap predicates(subjectIter->second);
+    
+    auto predicateIter(std::begin(predicates));
+    for (; predicateIter != std::end(predicates); ++predicateIter) {
+      term_id p(predicateIter->first);
+      TermSet objects(predicateIter->second);
+      
+      auto objectIter(std::begin(objects));
+      for (; objectIter != std::end(objects); ++objectIter) {
+        term_id o(*objectIter);
+        
+        triples.push_back(triple(s, p, o));
+      }
+    }
+  }
 }
--- src/Store.h
+++ src/Store.h
@@ -11,16 +11,30 @@
 
 #include <unordered_map>
 #include <unordered_set>
+#include <vector>
 #include "types.h"
 
 class Store {
 public:
-  bool addTriple(triple);
-  void PrintStatistics();
-private:
+  typedef std::vector<term_id> TermVector;
   typedef std::unordered_set<term_id> TermSet;
+  typedef std::unordered_map<term_id, TermSet> TermMap;
+  typedef std::vector<triple> TripleVector;
+
+  bool addTriple(triple);
+  void printStatistics();
+  void copySubjects(TermVector&);
+  void copyPredicates(TermVector&);
+  void copyObjects(TermVector&);
+  void copyTriples(TripleVector& triples);
+  
+  std::size_t size() { return tripleCount_; }
+
+private:
   typedef std::unordered_map<term_id, TermSet> PredicateMap;
   typedef std::unordered_map<term_id, PredicateMap> SubjectMap;
+  
+  std::size_t tripleCount_ = 0;
   SubjectMap storage_;
 };
 
