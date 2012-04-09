7086e7f2db09f76e80f8895d4b68704a9393e513
Norman Heino <norman.heino@gmail.com>
2012-04-09 22:46:14 +0200

    Add file-backed dictionary.

--- src/Dictionary.cc
+++ src/Dictionary.cc
@@ -1,66 +1,189 @@
-//
-//  Dictionary.cc
-//  grdfs
-//
-//  Created by Norman Heino on 11-12-10.
-//  Copyright (c) 2011 Norman Heino. All rights reserved.
-//
-
-#include <iostream>
-
 #include "Dictionary.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <cstring>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <stdexcept>
+#include <utility>
 
-term_id Dictionary::NextKey() {
-  return nextKey_++;
-}
+#define PAGE_SIZE 4096
+#define LOG2_PAGE_SIZE 12
+#define INIT_NUM_PAGES 32
+#define GOLDEN_RATIO 1.618f
 
-term_id Dictionary::Lookup(const std::string& lit) {
-  return Lookup(lit, false);
-}
+//////////////////////////////////////////////////////////////////////////////-
 
-term_id Dictionary::Lookup(const std::string& lit, const bool lit_hint) {
-  term_id key = map_[lit];
-  if (!key) {
-    key = NextKey();
-    
-    // For literal strings, the most significant bit will be 1
-    if (lit_hint) {
-      key |= literalMask;
-    }
-
-    map_[lit] = key;
-    keys_.insert(std::make_pair(key, lit));
+Dictionary::Dictionary(const std::string& fileName)
+{
+  int openResult = ::open(fileName.c_str(), O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+  if (openResult < 0) {
+    throw std::invalid_argument("File could not be opened or created.");
   }
-  
-  return key;
+  fd_ = openResult;
+  map(INIT_NUM_PAGES);
 }
 
-void Dictionary::Add(const std::string& lit) {
-  Lookup(lit);
+//////////////////////////////////////////////////////////////////////////////-
+
+Dictionary::~Dictionary()
+{
+  unmap();
+  close(fd_);
 }
 
-void Dictionary::Add(const std::string& lit, const bool lit_hint) {
-  Lookup(lit, lit_hint);
-}
+//////////////////////////////////////////////////////////////////////////////-
 
-std::string Dictionary::Find(const term_id key) const {
-  if (keys_.find(key) != keys_.end()) {
-    return keys_.find(key)->second;
+std::size_t Dictionary::writeHeader(Entry const& entry, std::size_t offset)
+{
+  // prevent string from crossing page boundary,
+  // moving it entirely to the next page
+  if ((offset >> LOG2_PAGE_SIZE) < (offset + sizeof(Entry) + entry.lsize) >> LOG2_PAGE_SIZE) {
+    offset = PAGE_SIZE * ((offset >> LOG2_PAGE_SIZE) + 1);
+    pos_ = map_ + offset;
   }
-  
-  return std::string("not found");
-}
 
-int Dictionary::Size() const {
-  return static_cast<int>(map_.size());
-}
-
-void Dictionary::Print() const {
-  for (auto iter = map_.begin(); iter != map_.end(); iter++) {
-    std::cout << iter->first << " => " << iter->second << std::endl;
+  // remap if necessary
+  if ((offset + sizeof(Entry) + entry.lsize) > size_) {
+    unmap();
+    map(floorf(GOLDEN_RATIO * numPages_));
+    offset = pos_ - map_;
   }
+
+  // write entry
+  writeValue(offset, entry);
+
+  // advance pointer
+  pos_ += sizeof(Entry);
+
+  return offset;
 }
 
-void Dictionary::PrintStatistics() const {
-  std::cout << "Dictionary with " << map_.size() << " entries.\n";
+//////////////////////////////////////////////////////////////////////////////-
+
+std::size_t Dictionary::writeLiteral(std::string const& lit, std::size_t offset)
+{
+  // write literal
+  lit.copy(map_ + offset, lit.size());
+
+  // advance pointer
+  pos_ += lit.size();
+
+  return offset;
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+Dictionary::Entry Dictionary::readEntry(KeyType const offset)
+{
+  return readValue<Entry>(offset);
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+std::string Dictionary::readLiteral(KeyType const offset, std::size_t length)
+{
+  return std::string(map_ + offset, length);
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+std::size_t Dictionary::hash(std::string const& str)
+{
+  std::size_t hash(0);
+  for (int i(0); i != str.size(); ++i) {
+    hash = hash * 101 + str[i];
+  }
+
+  return hash;
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+Dictionary::KeyType Dictionary::Lookup(std::string const& lit)
+{
+  std::size_t key = hasher_(lit);
+  auto it(ids_.find(key));
+  KeyType literalID;
+
+  if (it != std::end(ids_)) {
+    literalID = it->second;
+    std::ptrdiff_t parentOffset = literals_[literalID - 1];
+    Entry entry(0, parentOffset, 0);
+
+    do {
+      parentOffset = entry.next;
+      entry = readEntry(parentOffset);
+      if (entry.lsize == lit.size()) {
+        if (::memcmp(map_ + parentOffset + sizeof(Entry), lit.data(), entry.lsize) == 0) {
+          return entry.id;
+        }
+      }
+    } while (entry.next);
+
+    // overflow entry
+    std::ptrdiff_t offset = pos_ - map_;
+    writeValue(parentOffset + sizeof(KeyType), offset);
+    return writeEntry(lit, offset);
+  }
+
+  // new entry
+  std::ptrdiff_t offset = pos_ - map_;
+  literalID = writeEntry(lit, offset);
+  ids_[key] = literalID;
+  return literalID;
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+Dictionary::KeyType Dictionary::writeEntry(std::string const& lit, std::ptrdiff_t offset)
+{
+  // not found, create a new entry
+  KeyType literalID = nextKey_++;
+  Entry newEntry(literalID, 0, lit.size());
+  offset = writeHeader(newEntry, offset);
+  writeLiteral(lit, offset + sizeof(Entry));
+  literals_.push_back(offset);
+  return literalID;
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+std::string Dictionary::Find(KeyType key) const
+{
+  std::ptrdiff_t offset = literals_[key - 1] + 2 * sizeof(KeyType);
+  char* pos = map_ + offset;
+
+  // read size
+  std::size_t size;
+  ::memcpy(&size, pos, sizeof(size));
+  pos += sizeof(size);
+
+  return std::string(pos, size);
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+void Dictionary::unmap()
+{
+  ::munmap(map_, size_);
+}
+
+//////////////////////////////////////////////////////////////////////////////-
+
+void Dictionary::map(std::size_t numPages)
+{
+  std::ptrdiff_t offset = pos_ - map_;
+
+  if (::ftruncate(fd_, numPages * PAGE_SIZE - 1) != 0) {
+    throw std::runtime_error("Could not change file size.");
+  }
+  void* map = ::mmap(0, numPages * PAGE_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd_, 0);
+  if (map == MAP_FAILED) {
+    throw std::runtime_error("Could not map file.");
+  }
+  map_ = static_cast<char*>(map);
+  pos_ = map_ + offset;
+  size_ = numPages * PAGE_SIZE;
+  numPages_ = numPages;
 }
--- src/Dictionary.h
+++ src/Dictionary.h
@@ -1,41 +1,63 @@
-//
-//  Dictionary.h
-//  grdfs
-//
-//  Created by Norman Heino on 11-12-10.
-//  Copyright (c) 2011 Norman Heino. All rights reserved.
-//
+#ifndef DICTIONARY_H
+#define DICTIONARY_H
 
-#ifndef Dictionary_h
-#define Dictionary_h
-
-#include "types.h"
-
+#include <cstddef> // size_t
 #include <string>
-#include <vector>
 #include <unordered_map>
+#include <vector>
 
 class Dictionary {
 public:
-  Dictionary() : nextKey_(1) {}
-  term_id NextKey();
-  term_id Lookup(const std::string& lit);
-  term_id Lookup(const std::string& lit, const bool lit_hint);
-  void Add(const std::string& lit);
-  void Add(const std::string& lit, const bool lit_hint);
-  int Size() const;
-  void Print() const;
-  void PrintStatistics() const;
-  std::string Find(const term_id key) const;
-  static const term_id literalMask = (1UL << (sizeof(term_id) * 8 - 1));
+  typedef std::size_t KeyType;
+  Dictionary(const std::string& fileName);
+  ~Dictionary();
+  KeyType Lookup(std::string const& key);
+  std::string Find(KeyType key) const;
+  std::size_t Size() const { return 0; }
+  static const KeyType literalMask = (1UL << (sizeof(KeyType) * 8 - 1));
 private:
-  typedef std::unordered_map<std::string, term_id> LiteralMap;
-  typedef std::unordered_map<term_id, std::string> KeyMap;
+  typedef std::vector<KeyType> KeyVector;
+  typedef std::hash<std::string> StringHasher;
+  typedef std::unordered_map<KeyType, std::size_t> KeyMap;
+  // id, overflow, literal
+  typedef std::tuple<KeyType, KeyType, std::string> ValueType;
 
-  term_id nextKey_;
+  struct Entry {
+    KeyType id;
+    KeyType next;
+    std::size_t lsize;
+    Entry() : id(0), next(0), lsize(0) {};
+    Entry(KeyType id_, KeyType next_, std::size_t lsize_) : id(id_), next(next_), lsize(lsize_) {};
+  };
 
-  LiteralMap map_;
-  KeyMap keys_;
+  KeyMap ids_;
+  KeyVector literals_;
+  std::size_t size_;
+  std::size_t numPages_;
+  KeyType nextKey_ = 1;
+  std::string fileName_;
+  StringHasher hasher_;
+  int fd_;
+  char* map_ = nullptr;
+  char* pos_ = nullptr;
+
+  void map(std::size_t size);
+  void unmap();
+
+  template <typename T>
+  void writeValue(KeyType const offset, T const value) { ::memcpy(map_ + offset, &value, sizeof(T)); }
+
+  template <typename T>
+  T readValue(KeyType const offset) { T value; ::memcpy(&value, map_ + offset, sizeof(T)); return value; }
+
+  std::size_t writeHeader(Entry const& entry, std::size_t offset);
+  std::size_t writeLiteral(std::string const& lit, std::size_t offset);
+  Entry readEntry(KeyType const offset);
+  std::string readLiteral(KeyType const offset, std::size_t length);
+
+  KeyType writeEntry(std::string const& lit, std::ptrdiff_t offset);
+
+  std::size_t hash(std::string const& str);
 };
 
 #endif
